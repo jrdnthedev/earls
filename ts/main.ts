76fa2d306b58c773586142502db59bd7082832b8
smooooky <75350537+smooooky@users.noreply.github.com>
2024-04-28 20:37:56 -0400

    updated mobile menu animation

--- ts/main.ts
+++ ts/main.ts
@@ -7,14 +7,17 @@ close_btn?.addEventListener("click", toggle);
 function toggle() {
     isToggle = !isToggle;
     const body = document.getElementById('body');
-    const nav = document.getElementById('main_nav');
+    const nav = document.getElementById('hamburger_menu');
+    const menu = document.getElementById('main_nav');
 
     if(isToggle) {
         body?.classList.add('scroll-lock');
         nav?.classList.add('active');
+        menu?.classList.add('active');
     } else {
         body?.classList.remove('scroll-lock');
         nav?.classList.remove('active');
+        menu?.classList.remove('active');
     }
 
 }
